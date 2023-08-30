2655e175a1d1526b962a868f09485ebfb3135817
Faroq azadh <faroqazadh@gmail.com>
2023-08-30 18:19:25 +0330

    six project

---
+++ 6/script.js
@@ -0,0 +1,29 @@
+const container = document.getElementById('container')
+const colors = ['#8d0801', '#bf0603', '#f4d58d', '#708d81', '#001427','#003049','#d62828','#f77f00','#fcbf49','#eae2b7']
+const SQUARES = 600
+
+for(let i = 0; i < SQUARES; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+
+    square.addEventListener('mouseout', () => removeColor(square))
+
+    container.appendChild(square)
+}
+
+function setColor(element) {
+   const color = getRandomColor()
+   element.style.background = color
+   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+   element.style.background = '#1d1d1d'
+   element.style.boxShadow = '0 0 2px #000'
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
